e88382f9d0dd508cd74e5545022598224d95572b
Francis Authority <66372007+france-auth@users.noreply.github.com>
2023-01-04 19:24:44 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+
+ /** 
+
+   *main - This prints the alphabet from A-z 
+
+   *Return: Always 0 
+
+   */ 
+
+ int main(void) 
+
+ { 
+
+         char a = 'a'; 
+
+  
+
+         while (a <= 'z') 
+
+         { 
+
+                 putchar(a); 
+
+                 a++; 
+
+  
+
+         } 
+
+  
+
+         putchar('\n'); 
+
+         return (0); 
+
+  
+
+ }
