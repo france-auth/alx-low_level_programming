b16866dbe782517f1419f0c1d7467fddf7355781
Francis <franceauth@gmail.com>
2023-01-04 11:21:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-   * main - Entry 
+ 	* main - Entry 
 
-   * Return: Always 0 
+	* Return: Always 0 
 */ 
 
-int main(void) 
-
- { 
+int main(void)
+ {
 
          puts("\"Programming is like building a multilingual puzzle"); 
 
